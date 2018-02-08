1af7baa86c71e38aa6ff580d63717580468d5556
Anna Scholtz <anna@scholtzan.net>
2018-02-07 16:43:47 -0800

    Retrieve names of external dependencies

---
+++ README.md
@@ -0,0 +1,8 @@
+[todo]
+
+
+## Installation
+
++ Download the source code of the tool
++ Install all requirements: `pip install -r requirements.txt`
++ [todo]
---
+++ src/cli.py
@@ -0,0 +1,18 @@
+import click
+import vulnerability_analysis
+
+
+# todo: list of URLs to be analyzed
+@click.command()
+@click.option('--url', help='URL to a github repository')    
+@click.option('--path', help='Path to a local project directory')
+def main(url, path):
+    # analyze source code of provided project
+    # todo
+
+    print "Start analysis"
+
+    print vulnerability_analysis.get_dependencies(path)
+
+if __name__ == '__main__':
+    main()
