2d0c117bddb335dd24b69b351cce86aae3a38770
Anna Scholtz <anna@scholtzan.net>
2018-02-27 15:38:47 -0800

    Code refactoring

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,10 @@
 import click
-import vulnerability_analysis
-import vulnerabilities
-import crawler
+from vulnerability_analysis import VulnerabilityAnalyzer
+from github_repo_handler import GithubRepoHandler
 import uuid
 from report import Report
 
 
-# todo: list of URLs to be analyzed
 @click.command()
 @click.option('--url', help='URL to a github repository')
 @click.option('--path', help='Path to a local project directory')
@@ -14,36 +12,63 @@ from report import Report
 @click.option('--push', is_flag=True, default=False, help='Automatically creates pull-request with changes')
 @click.option('--html', help='Create html report in provided file')
 def main(url, path, replace, push, html):
+    """
+    Start vulnerability analysis using command line tool.
+    :param url: URL to a github repository
+    :param path: path to a locally stored project
+    :param replace: flag of whether detected vulnerabilities
+    should be automatically replaced with safe alternative
+    :param push: flag indicating whether pull-request should
+    automatically be created
+    :param html: path to html file which will contain report
+    """
     # analyze source code of provided project
     print "Start analysis"
 
+    # store remote repo locally in /tmp
     local_repo_path = '/tmp/' + str(uuid.uuid4())
-    local_repo = None
-    remote_repo = None
+    gh_handler = None
 
     if url:
-        (local_repo, remote_repo, orig_repo) = crawler.download_repository(url, local_repo_path)
-        crawler.get_repository_metrics(remote_repo)
+        # download remote repo
+        gh_handler = GithubRepoHandler(url)
+        gh_handler.download_repository(local_repo_path)
+        gh_handler.get_repository_metrics()
     elif path:
+        # analyze local repo
         local_repo_path = path
-        
-    detected_vulnerabilities = vulnerability_analysis.vulnerability_analysis_in_path(local_repo_path)
-    vulnerable_dependencies = vulnerability_analysis.vulnerable_dependencies_in_path(local_repo_path)
+
+    vulnerability_analyzer = VulnerabilityAnalyzer(local_repo_path)
+    # check for vulnerable functions and vulnerable dependencies
+    vulnerability_analyzer.analyze()
+
+    vulnerable_functions = vulnerability_analyzer.detected_vulnerable_functions
+    # todo: add to report and update
+    vulnerable_dependencies = vulnerability_analyzer.detected_vulnerable_imports
 
     if replace:
+        # automatically replace detected vulnerabilities if available
         print "Replace detected vulnerabilities"
-        vulnerability_analysis.replace_vulnerabilities(detected_vulnerabilities)
+        vulnerability_analyzer.replace_vulnerabilities()
 
-        if push and remote_repo:
-            print "Create pull-request"
-            crawler.push_updates(orig_repo, local_repo, "bugrevelio@byom.de", "bugrevelio", "Test", "Test body", "bugrevelio:master", "master")
+    report = Report(vulnerable_functions, None, None, replace)
 
-    report = Report(detected_vulnerabilities, None, None, replace)
+    # automatically create pull request
+    if push:
+        print "Create pull-request"
+
+        # todo: include report in pull-request
+        gh_handler.push_updates("bugrevelio@byom.de",
+                                "bugrevelio",
+                                "Vulnerability check",
+                                report.plain_text_report(),
+                                "bugrevelio:master",
+                                "master")
+
     print report.plain_text_report()
 
     if html:
         report.html_report(html)
-
 
     # todo: delete downloaded repo
 
--- src/vulnerabilities.py
+++ src/vulnerabilities.py
@@ -1,21 +1,28 @@
 import json
 from safety_db import INSECURE_FULL
 
-def get_all_vulnerable_functions(area = "crypto"):
-    """
-    Parses the .json file containing known vulnerabilities.
-    :param area: file name
-    :return: parsed dictionary with vulnerabilities
-    """
-    filename = "data/{area}.json".format(area = area)
-    with open(filename) as json_file:
-        json_data = json.load(json_file)
 
-    return json_data
+class VulnerabilityDB:
+    def get_all_vulnerable_functions(self, area="crypto"):
+        """
+        Parses the .json file containing known vulnerabilities.
+        :param area: file name
+        :return: parsed dictionary with vulnerabilities
+        """
+        filename = "data/{area}.json".format(area=area)
+        with open(filename) as json_file:
+            json_data = json.load(json_file)
 
+        return json_data
 
-def get_vulnerable_dependency(name):
-    if name in INSECURE_FULL:
-        return INSECURE_FULL[name]
-    else:
-        return None
+    def get_vulnerable_dependency(self, name):
+        """
+        Returns all information regarding a vulnerable dependency.
+        If no information is available then `None` is returned.
+        :param name: name of the dependeny
+        :return: todo
+        """
+        if name in INSECURE_FULL:
+            return INSECURE_FULL[name]
+        else:
+            return None
